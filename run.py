c5235461cb90647310a362f62649d5edff6720d5
AlistairDriscoll <pallythecoder@gmail.com>
2023-11-21 18:18:04 +0000

    add to game function, complete word lists

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import words
-
-print("Welcome to Hangman!")
+import random
 
 def menu_page():
     """
@@ -26,7 +25,7 @@ def rules_page():
 Hangman is a game of guessing.
 You can choose a difficulty rating and you will be given an easier or harder word to guess depending on what you have chosen.
 At first you will not know any of the letters and will have to guess what they are.
-Make sure to choose carefully as you will only have ten guesses until you lose the game!
+Make sure to choose carefully as you will only have seven guesses until you lose the game!
 If you guess the full word before you run out of guesses, you win!
 """)
     
@@ -35,14 +34,32 @@ If you guess the full word before you run out of guesses, you win!
         print("Incorrect input, please select b to go to the game.")
         go_to_menu = input("Select here...")
 
-    
-        
-
-
 
 def game_page():
-    """Function to start the game itself"""
-    print("Here is the game")
+    """
+    Function to start the game itself
+    """
+
+    # Starts by asking the user for their difficulty rating, with a while loop to make sure they choose a valid option
+
+    print("To start, select a difficulty rating! 1 is for easy mode, 2 is for intermediate, and 3 is for hard mode.")
+    difficulty_choice = input("Please select here...")
+
+    while difficulty_choice not in ["1", "2", "3"]:
+        print("Incorrect choice, select either 1 for easy difficulty, 2 for intermediate or 3 for hard difficulty")
+        difficulty_choice = input("Please select here...")
+        difficulty_choice = int(difficulty_choice)
+        
+    if difficulty_choice == 1:
+        word_list = words.easy_words
+    elif difficulty_choice == 2:
+        word_list = words.medium_words
+    else:
+        word_list = words.hard_words
+
+    round_word = random.choice(word_list)
+
+    
 
 
 def main():
@@ -65,10 +82,6 @@ def main():
     print("Thank you for playing hangman!")
         
     
-    
-
-
-
 main()
 
 
