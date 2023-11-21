f6b87606c60655070ff5fd3c1077ffee07e0ac4f
AlistairDriscoll <pallythecoder@gmail.com>
2023-11-21 11:26:27 +0000

    add words file, add quit game option, correct name of main function and add edits

--- run.py
+++ run.py
@@ -1,20 +1,27 @@
+import words
+
 print("Welcome to Hangman!")
 
 def menu_page():
-    """Function that shows the menu, the first part user will see when they start the game, once the game ends it will return to this bit"""
+    """
+    Function that shows the menu, the first part user will see when they start the game, once the game ends it will return to this bit
+    """
     
-    print(f"\nPlease press 1 to look at the rules or 2 to start the game.\n")
+    print(f"\nPlease press 1 to look at the rules, 2 to start the game, or 3 to exit the game.\n")
     menu_choice = input("Choose an option here...")
 
-    while menu_choice not in ["1", "2"]:
-        print(f"Incorrect choice, please press 1 to look at the rules or 2 to start the game.\n")
+    while menu_choice not in ["1", "2", "3"]:
+        print(f"Incorrect choice, please press 1 to look at the rules, 2 to start the game, or 3 to exit the game.\n")
         menu_choice = input("Choose an option here...")
-
+        
     return menu_choice
 
 
 def rules_page():
-    """Function to display the rules"""
+    """
+    Function to display the rules
+    """
+
     print("""
 Hangman is a game of guessing.
 You can choose a difficulty rating and you will be given an easier or harder word to guess depending on what you have chosen.
@@ -28,8 +35,8 @@ If you guess the full word before you run out of guesses, you win!
         print("Incorrect input, please select b to go to the game.")
         go_to_menu = input("Select here...")
 
-    if go_to_menu.lower() == "b":
-        menu_page()
+    
+        
 
 
 
@@ -37,22 +44,31 @@ def game_page():
     """Function to start the game itself"""
     print("Here is the game")
 
-def where_next_function():
+
+def main():
     """
     Function that controls where the user goes next, depending on their option, making sure they havent typed in an invalid choice
     """
     menu_choice = menu_page()
 
+    while int(menu_choice) != 3:
+        if int(menu_choice) == 1:
+            print("You chose to see the rules!")
+            rules_page()
+            
+        elif int(menu_choice) == 2:
+            print("Your chose to start the game!")
+            game_page()
+        
+        menu_choice = menu_page()
 
-    if int(menu_choice) == 1:
-        print("You chose to see the rules!")
-        rules_page()
-    elif int(menu_choice) == 2:
-        print("Your chose to start the game!")
-        game_page()
+    print("Thank you for playing hangman!")
+        
+    
+    
 
 
 
-
+main()
 
 
---
+++ words.py
@@ -0,0 +1,7 @@
+"""
+The word lists depending on difficulty
+"""
+
+easy_words = ["hello"]
+medium_words = ["about"]
+hard_words = ["quintissential"]
